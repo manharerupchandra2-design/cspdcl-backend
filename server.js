84b0ba58e839b4250a616cf4672c9fae13094630
Rupchandra <manharerupchandra2@gmail.com>
2026-04-30 08:19:47 +0530

    fixed port for deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/readings", readingRoutes);
 app.use("/api/bills", billRoutes);
 app.use("/api/meters",meterRoutes);
 app.use("/api/user", userRoutes)
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
