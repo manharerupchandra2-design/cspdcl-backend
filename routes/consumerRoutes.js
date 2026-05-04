870c741101cc2596da496af285da6c690e90d45e
Rupchandra <manharerupchandra2@gmail.com>
2026-05-05 05:03:04 +0530

    New UPDATE

--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {
   addConsumer,
   getAllConsumers,
+  getAllConsumersDetail,
   getSingleConsumer,
   removeConsumer
 } = require("../controllers/consumerController");
 
 router.post("/", addConsumer);
 router.get("/", getAllConsumers);
+router.get("/getAllConsumersDetail", getAllConsumers);
 router.get("/:id", getSingleConsumer);
 router.delete("/:id",removeConsumer);
 
