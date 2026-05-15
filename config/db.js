7ffd6525f0b5cc804b1c1e28f1e2cd9685358c2f
Rupchandra <manharerupchandra2@gmail.com>
2026-05-15 23:35:44 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,34 +1,35 @@
 
 require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const db = mysql.createPool(
+ process.env.MYSQL_URL
+);
 
-// const pool=mysql.createPool({
+
+console.log("MYSQL URL =", process.env.MYSQL_URL);
+
+
+// const db=mysql.createPool({
 //   host:"localhost",
 //   user:"root",
 //   password:"Rup1432@",
 //   database:"cspdcl_billing"
 // })
 
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.log("DB ERROR ❌", err);
-  } else {
-    console.log("DB Connected ✅");
+async function checkDB() {
+  try {
+    const conn = await db.getConnection();
+
+    console.log("DB Connected");
+
     conn.release();
+  } catch (err) {
+    console.log("DB ERROR", err);
   }
-});
+}
 
-
-const db = pool.promise();
+checkDB();
 
 module.exports = db;
 
