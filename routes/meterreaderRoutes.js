2e0eef2e04f6a65b27ab326265a138cf7dca00fb
Rupchandra <manharerupchandra2@gmail.com>
2026-05-16 07:48:08 +0530

    updated meterreader column

--- routes/meterreaderRoutes.js
+++ routes/meterreaderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { meterreaderLogin, meterreaderSignup } = require("../controllers/userController");
+const { meterreaderLogin, meterreaderSignup } = require("../controllers/meterreaderController");
 
 router.post("/login", meterreaderLogin);
 router.post("/signup", meterreaderSignup);
