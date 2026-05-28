1adf1ad98c173986bc4cf0d78a80aac96a49bf83
Rupchandra <manharerupchandra2@gmail.com>
2026-05-28 08:20:57 +0530

    updated query

--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -11,7 +11,7 @@ const {
 
 // router.post("/", addConsumer);
 router.get("/", getAllConsumers);
-router.post("/getConsumersPreviousBill/:id", getConsumersPreviousBill);
+router.get("/getConsumersPreviousBill/:id", getConsumersPreviousBill);
 // router.get("/:id", getSingleConsumer);
 // router.delete("/:id",removeConsumer);
 
