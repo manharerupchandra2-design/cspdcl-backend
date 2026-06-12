75ee83780c6a0c05ef1f007c8d805fd252e36aad
Rupchandra <manharerupchandra2@gmail.com>
2026-06-12 23:33:54 +0530

    update1

--- routes/readingRoutes.js
+++ routes/readingRoutes.js
@@ -6,12 +6,12 @@ const upload = require('../middleware/upload');
 const router = express.Router();
 const { verifyToken } = require("../middleware/authMiddleware");
 const {
-  setReading, getReadingHistory, editReading
+  setReading, getReadingHistory
 } = require("../controllers/readingController");
 
 router.get("/history", getReadingHistory)
 router.post("/:consumerId", upload.single('meter_photo'), setReading);
 
-router.put("/editReading/:reading_id", editReading)
+// router.put("/editReading/:reading_id", editReading)
 
 module.exports = router;
