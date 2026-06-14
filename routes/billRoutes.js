1134369ba39290914bfb803801f050ff0570feeb
Rupchandra <manharerupchandra2@gmail.com>
2026-06-14 06:36:32 +0530

    zone add

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {setBilling } = require("../controllers/billController");
 
 router.post("/:reading_id", setBilling);
+// router.get("/history", getBillHistory);
 
 module.exports = router;
 
--- routes/consumerRoutes.js
+++ routes/consumerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const { verifyToken } = require("../middleware/authMiddleware")
 const {
   getConsumers,
   getConsumerDetail
