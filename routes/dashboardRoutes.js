00477bf931383105b79b93f48277421421db9cc5
Rupchandra <manharerupchandra2@gmail.com>
2026-06-14 06:47:59 +0530

    getDashboard add token header

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const {verifyToken} = require("../middleware/authMiddleware")
 const { getDashboard } = require("../controllers/dashboardController");
 
-router.get('/', getDashboard);
+router.get('/', verifyToken, getDashboard);
 
 
 
