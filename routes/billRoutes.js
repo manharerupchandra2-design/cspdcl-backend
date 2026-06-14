a0b2dc135c93949fd26040dfec2127b0912f8c93
Rupchandra <manharerupchandra2@gmail.com>
2026-06-14 06:40:52 +0530

    order maintain bill

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {setBilling } = require("../controllers/billController");
+const {getBillHistory, setBilling  } = require("../controllers/billController");
 
+
+router.get("/history", getBillHistory);
 router.post("/:reading_id", setBilling);
-// router.get("/history", getBillHistory);
+
 
 module.exports = router;
 
